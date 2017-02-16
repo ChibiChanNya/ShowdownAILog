e781b57ff7f3e70c067767baf3c1d176e3d6e6a3
ChibiChanNya <chibi_theone@yahoo.com>
2017-02-15 23:27:46 -0600

    Basic game-state tracking

--- start.js
+++ start.js
@@ -1,13 +1,18 @@
 /**
  * Created by Chibi on 06/02/17.
  */
+/*jslint node:true*/
 
 var sjsc = require('sockjs-client-ws');
 var request = require('request');
 
+var state= {};
+
+
 
 var constructSocket = function () {
     return  sjsc.create("http://localhost:8000/showdown");
+
 };
 
 var client = constructSocket();
@@ -22,7 +27,7 @@ client.on('connection', function () {
 client.on('data', function (msg) {
     //Received Data
     console.log("Received <<<", msg, "\n");
-    // processMessage(msg);
+    processMessage(msg);
 });
 
 client.on('error', function (e) {
@@ -38,39 +43,79 @@ var send = function(message){
 function processMessage(message){
     let chunks = message.split("|");
     // console.log("chunks:",chunks, "\n");
-    if(chunks[1] == "init"){
-        send("|Hello nyans");
+    switch(chunks[1]){
+
+        case "challstr":
+            let challstr = chunks[2]+'|'+chunks[3];
+            console.log("challstr is:", challstr);
+            login(challstr);
+            break;
+
+        case "updatechallenges":
+            let challenges = JSON.parse(chunks[2]);
+            console.log(challenges);
+            let challengers = challenges.challengesFrom;
+            let first_challenger = Object.keys(challengers);
+            send('|/accept '+ first_challenger);
+            break;
+
+        case "request":
+            let mySide = JSON.parse(chunks[2]);
+            console.log(JSON.stringify(mySide, null, 4));
+            state[chunks[0]].availible_moves = mySide.active[0].moves;
+            state[chunks[0]].my_team = mySide.side.pokemon;
+            state[chunks[0]].active_pokemon= myside.side.pokemon[0];
+            state[chunks[0]].active_pokemon.hp = myside.side.pokemon[0].condition.substr(0,3);
+            break;
+
+        // case "player":
+        //     let enemySide=
+        //     break;
+
+    }
+    if(chunks[1] == "challstr"){
+
     }
 }
 
 
 
+
+
 /// MAIN FLOW
-var options = { method: 'POST',
-    //Local Server
-    url: 'https://play.pokemonshowdown.com/~~localhost/action.php',
-    //Official Servers
-    // url: 'https://play.pokemonshowdown.com/~~showdown/action.php',
-    headers:
-        { 'postman-token': '881c798c-6e69-7576-f52a-49eb72948ada',
-            'cache-control': 'no-cache',
-            'content-type': 'application/x-www-form-urlencoded' },
-    form: "act=login&name=PorygonDelta&pass=porybot215&challstr=4%7C605d6c793ce95191f33c6fcd0afb2fe47f00c9b889db6312cd20c3e29f39fedff8cf29e604db30e54a1bf45f877f5ee3b083d7d4f4b5dc418137d9fec58e55232cd297c637bbc71fda8472fa77c72376e4ff53f7407c826e805d50e0be0590af45a675a3ea97e807e62a9523ac7271ec45767e6976c6502cadfd711bcc4301dc%7C653445mqf9469345mqf9628645mqf9876345mqf93012"
+function login(challstr){
+
+    var options = { method: 'POST',
+        //Local Server
+        url: 'https://play.pokemonshowdown.com/~~localhost/action.php',
+        //Official Servers
+        // url: 'https://play.pokemonshowdown.com/~~showdown/action.php',
+        headers:
+            { 'postman-token': '881c798c-6e69-7576-f52a-49eb72948ada',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded' },
+        form: "act=login&name=PorygonDelta&pass=porybot215&challstr="+challstr
+    };
+
+    console.log("FORM:", options.form);
+
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        // console.log("HTTP RESPONSE: ", body);
+        let user = JSON.parse(body.substr(1)).curuser;
+        let assertion = JSON.parse(body.substr(1)).assertion;
+        console.log("ASSERTION:", assertion);
+        let login_message = "|/trn "+user.username+",0,"+ assertion ;
+        send("|/avatar 42,1");
+        send("|/autojoin");
+        send(login_message);
+    });
+
 };
 
 
-
-request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    let user = JSON.parse(body.substr(1)).curuser;
-    let assertion = JSON.parse(body.substr(1)).assertion;
-    let login_message = "|/trn "+user.username+",0,"+ assertion ;
-    send("|/avatar 42,1");
-    send("|/autojoin");
-    send(login_message);
-
-});
 
 
 
